a0d3bd206e3676e529424572dfb2ef1d1b8ecf55
lanco <lancowhatyouneed@gmail.com>
2020-12-13 19:57:18 +0800

    solve 20

---
+++ 20.go
@@ -0,0 +1,113 @@
+package leetcode_offer
+
+/**
+请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。例如，字符串"+100"、"5e2"、"-123"、"3.1416"、"-1E-16"、"0123"都表示数值，但"12e"、"1a3.14"、"1.2.3"、"+-5"及"12e+5.4"都不是。
+*/
+
+/**
+找出规律
+字符串可能的+-、数字0-9，小数点，幂符号
+
+有限状态机
+
+循环代码简单，主要遍历这个字符串，根据状态进行转移，如果提前得到false直接退出
+*/
+type State int
+type CharType int
+
+const (
+	STATE_INITIAL           State = iota //起始的空格
+	STATE_INT_SIGN                       //符号位
+	STATE_INTEGER                        //整数部分
+	STATE_POINT                          //小数点
+	STATE_POINT_WITHOUT_INT              //小数点前
+	STATE_FRACTION                       //
+	STATE_EXP                            //
+	STATE_EXP_SIGN                       //
+	STATE_EXP_NUMBER                     //
+	STATE_END                            //
+)
+
+const (
+	CHAR_NUMBER  CharType = iota //整数部分
+	CHAR_EXP                     //指数部分
+	CHAR_POINT                   //小数点
+	CHAR_SIGN                    //符号位
+	CHAR_SPACE                   //空格
+	CHAR_ILLEGAL                 //非法字符串
+)
+
+func toCharType(ch byte) CharType {
+	switch ch {
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		return CHAR_NUMBER
+	case 'e', 'E':
+		return CHAR_EXP
+	case '.':
+		return CHAR_POINT
+	case '+', '-':
+		return CHAR_SIGN
+	case ' ':
+		return CHAR_SPACE
+	default:
+		return CHAR_ILLEGAL
+	}
+}
+
+func isNumber(s string) bool {
+	transfer := map[State]map[CharType]State{
+		STATE_INITIAL: map[CharType]State{
+			CHAR_SPACE:  STATE_INITIAL,
+			CHAR_NUMBER: STATE_INTEGER,
+			CHAR_POINT:  STATE_POINT_WITHOUT_INT,
+			CHAR_SIGN:   STATE_INT_SIGN,
+		},
+		STATE_INT_SIGN: map[CharType]State{
+			CHAR_NUMBER: STATE_INTEGER,
+			CHAR_POINT:  STATE_POINT_WITHOUT_INT,
+		},
+		STATE_INTEGER: map[CharType]State{
+			CHAR_NUMBER: STATE_INTEGER,
+			CHAR_EXP:    STATE_EXP,
+			CHAR_POINT:  STATE_POINT,
+			CHAR_SPACE:  STATE_END,
+		},
+		STATE_POINT: map[CharType]State{
+			CHAR_NUMBER: STATE_FRACTION,
+			CHAR_EXP:    STATE_EXP,
+			CHAR_SPACE:  STATE_END,
+		},
+		STATE_POINT_WITHOUT_INT: map[CharType]State{
+			CHAR_NUMBER: STATE_FRACTION,
+		},
+		STATE_FRACTION: map[CharType]State{
+			CHAR_NUMBER: STATE_FRACTION,
+			CHAR_EXP:    STATE_EXP,
+			CHAR_SPACE:  STATE_END,
+		},
+		STATE_EXP: map[CharType]State{
+			CHAR_NUMBER: STATE_EXP_NUMBER,
+			CHAR_SIGN:   STATE_EXP_SIGN,
+		},
+		STATE_EXP_SIGN: map[CharType]State{
+			CHAR_NUMBER: STATE_EXP_NUMBER,
+		},
+		STATE_EXP_NUMBER: map[CharType]State{
+			CHAR_NUMBER: STATE_EXP_NUMBER,
+			CHAR_SPACE:  STATE_END,
+		},
+		STATE_END: map[CharType]State{
+			CHAR_SPACE: STATE_END,
+		},
+	}
+	state := STATE_INITIAL
+	for i := 0; i < len(s); i++ {
+		typ := toCharType(s[i])
+		if _, ok := transfer[state][typ]; !ok {
+			return false
+		} else {
+			state = transfer[state][typ]
+		}
+	}
+	return state == STATE_INTEGER || state == STATE_POINT || state == STATE_FRACTION || state == STATE_EXP_NUMBER || state == STATE_END
+}
